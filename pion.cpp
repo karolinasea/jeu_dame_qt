f0a2d3f94a7811d3e1c10f30089ca6d188a6d350
karolinasea <46023630+karolinasea@users.noreply.github.com>
2019-05-29 13:29:41 -0700

    Update pion.cpp
    fixed issue selecting

--- pion.cpp
+++ pion.cpp
@@ -1,79 +1,102 @@
-#include "pion.hpp"
-#include "game.hpp"
+#include "pion.h"
+#include "game.h"
+#include "generateur.h"
 #include <QDebug>
 #include <typeinfo>
 
 
-Pion::Pion (Couleur team,QGraphicsItem *parent) : QGraphicsPixmapItem(parent), side(team), isPlaced(true), isDame(false)
+Pion::Pion (Couleur team, QGraphicsItem *parent) : QGraphicsPixmapItem(parent), side(team), isPlaced(true), isDame(false)
 {
     if(side == Couleur::Blanc)
-        setPixmap(QPixmap(":/images/pawn1.png"));
+        setPixmap(QPixmap("C:\\Users\\carole\\Documents\\build-CHECKERS2-Desktop_Qt_5_12_2_MinGW_32_bit2-Debug\\debug\\pawn1.png"));
     else
-        setPixmap(QPixmap(":/images/pawn.png"));
+        setPixmap(QPixmap("C:\\Users\\carole\\Documents\\build-CHECKERS2-Desktop_Qt_5_12_2_MinGW_32_bit2-Debug\\debug\\pawn.png"));
 
 }
 
 void Pion::conversionDame ()
 {
     if(side == Couleur::Blanc)
-        setPixmap(QPixmap(":/images/dame_blanche.png"));
-    else
-        setPixmap(QPixmap(":/images/dame_noire.png"));
-
+    {
+        setPixmap(QPixmap("C:\\Users\\carole\\Documents\\build-CHECKERS2-Desktop_Qt_5_12_2_MinGW_32_bit2-Debug\\debug\\dame_blanche.png"));
+    }
+    else if(side == Couleur::Noir)
+    {
+        setPixmap(QPixmap("C:\\Users\\carole\\Documents\\build-CHECKERS2-Desktop_Qt_5_12_2_MinGW_32_bit2-Debug\\debug\\dame_noire.png"));
+    }
 }
 
 void Pion::mousePressEvent(QGraphicsSceneMouseEvent *event)
 {
-    //si deja mangee ou pas le tour de la couleur
-    if((!getIsPlaced() )|| ( (currentBox->game->getTurn() != this->getSide())&& (!currentBox->game->pieceToMove)) )
-        return;
+    //selecting pieces
+        if(currentBox->game->pieceToMove == nullptr)
+        {
+            currentBox->game->pieceToMove = this;
+            if ((currentBox->game->getTurn()==Couleur::Noir && currentBox->game->pieceToMove->getSide()==Couleur::Blanc) || (currentBox->game->getTurn()==Couleur::Blanc && currentBox->game->pieceToMove->getSide()==Couleur::Noir))
+            {
+                qDebug()<<"wrong color compare to the turn 2";
+                currentBox->game->pieceToMove->getCurrentBoite()->resetOriginalColor();
+                currentBox->game->pieceToMove->decolor();
+                currentBox->game->pieceToMove = nullptr;
+                //event->ignore();
+            }
+            else
+            {
+                qDebug()<<"else";
+                currentBox->game->pieceToMove->getCurrentBoite()->setCouleur(Qt::red);
+                currentBox->game->pieceToMove->moves();
+            }
+        }
 
-    //Deselect
-    if(this == currentBox->game->pieceToMove){
-        currentBox->game->pieceToMove->getCurrentBoite()->resetOriginalColor();
-        currentBox->game->pieceToMove->decolor();
-        currentBox->game->pieceToMove = nullptr;
-       return;
-    }
-
-    //selecting
-    if(!currentBox->game->pieceToMove){
-
-        currentBox->game->pieceToMove = this;
-        currentBox->game->pieceToMove->getCurrentBoite()->setCouleur(Qt::red);
-        currentBox->game->pieceToMove->moves();
-    }
-    //Consuming counterPart of the boite   UTILITé ??
-    /*else if(this->getSide() != currentBox->game->pieceToMove->getSide()){
-        qDebug()<<"oui ";
-        this->getCurrentBoite()->mousePressEvent(event);
-    }*/
-
-
+        if(currentBox->game->pieceToMove != nullptr)
+        {
+            currentBox->game->pieceToMove->getCurrentBoite()->resetOriginalColor();
+            currentBox->game->pieceToMove->decolor();
+            currentBox->game->pieceToMove = this;
+            if ((currentBox->game->getTurn()==Couleur::Noir && currentBox->game->pieceToMove->getSide()==Couleur::Blanc) || (currentBox->game->getTurn()==Couleur::Blanc && currentBox->game->pieceToMove->getSide()==Couleur::Noir))
+            {
+                qDebug()<<"wrong color compare to the turn";
+                currentBox->game->pieceToMove->getCurrentBoite()->resetOriginalColor();
+                currentBox->game->pieceToMove->decolor();
+                currentBox->game->pieceToMove = nullptr;
+                //event->ignore();
+            }
+            else
+            {
+                qDebug()<<"else";
+                currentBox->game->pieceToMove->getCurrentBoite()->setCouleur(Qt::red);
+                currentBox->game->pieceToMove->moves();
+            }
+        }
 }
-
 
 void Pion::moves()
 {
-    location.clear(); 
-    boitePionEnnemi.clear(); 
+    location.clear(); /*Removes all items from the list location which is an attribute of class Pion*/
+    boitePionEnnemi.clear(); /*Removes all items from the list boitePionEnnemi which is an attribute of class Pion*/
 
     int row = this->getCurrentBoite()->rowLoc; //get the row number of the box on which the pawn is placed
     char taille = currentBox->game->nbCases; //nbCases is the size of the board (8x8 or 10x10..)
     int col = this->getCurrentBoite()->colLoc; //get the column number of the box on which the pawn is placed
-    
-    gene = new generateur(taille, row, col, currentBox->game->tab_damier, this->getSide());
+
+    //tab_damier est le nom d'un attribut de game et est de type QVector<Boites>
+    //tab_damier est la liste des boites du damier ???? not sure
+    //gene = new generateur(taille, row, col, currentBox->game->tab_damier, this->getSide());
+    //rajout d'un attribut a generateur pour lui dire si le pion est une dame ou non
+    gene = new generateur(taille, row, col, currentBox->game->tab_damier, this->getSide(), this->isDame);
     gene->remplirListes();
 
-    //return ;
+    return ;
+    /*stops the fonction to go to remplirListes and then remplirListes colors the boites/cases
+    sur lesquelles le pion selectionne peut aller*/
 
     if(this->getSide() == Couleur::Blanc)
     {
         if(col > 1 && row > 1 && currentBox->game->tab_damier[(row-1)*taille+col-1]->getPionCouleur() == Couleur::Noir)
-        {  
+        {
             // qqch a manger a gauche ?
             if(!currentBox->game->tab_damier[(row-2)*taille+col-2]->getHasPion())
-            { 
+            {
                 // on verifie ya pas de pion sur point de chute
                 location.append(currentBox->game->tab_damier[(row-2)*taille+col-2]);
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row-1)*taille+col-1]);
@@ -81,10 +104,10 @@ void Pion::moves()
             }
         }
         if(col < taille-2 && row > 1 && currentBox->game->tab_damier[(row-1)*taille+col+1]->getPionCouleur() == Couleur::Noir)
-        { 
+        {
             //qqch a manger a droite ?
             if(!currentBox->game->tab_damier[(row-2)*taille+col+2]->getHasPion())
-            { 
+            {
                 // on verifie ya pas de pion sur point de chute
                 location.append(currentBox->game->tab_damier[(row-2)*taille+col+2]);
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row-1)*taille+col+1]);
@@ -93,10 +116,10 @@ void Pion::moves()
         }
 
         if(col > 1 && row < taille-2 && currentBox->game->tab_damier[(row+1)*taille+col-1]->getPionCouleur() == Couleur::Noir)
-        {  
+        {
             //qqch a manger a gauche arriere ?
             if(!currentBox->game->tab_damier[(row+2)*taille+col-2]->getHasPion())
-            { 
+            {
                 // on verifie ya pas de pion sur point de chute
                 location.append(currentBox->game->tab_damier[(row+2)*taille+col-2]);
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row+1)*taille+col-1]);
@@ -104,10 +127,10 @@ void Pion::moves()
             }
         }
         if(col <taille-2 && row < taille-2 && currentBox->game->tab_damier[(row+1)*taille+col+1]->getPionCouleur() == Couleur::Noir)
-        {  
+        {
             // qqch a manger a droite arriere ?
             if(!currentBox->game->tab_damier[(row+2)*taille+col+2]->getHasPion())
-            { 
+            {
                 // on verifie ya pas de pion sur point de chute
                 location.append(currentBox->game->tab_damier[(row+2)*taille+col+2]);
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row+1)*taille+col+1]);
@@ -120,7 +143,7 @@ void Pion::moves()
         {
             //checks that row and col are in the board and not bigger and if case avt droite n'as pas de pion
             if(row > 0 && col < taille-1 && (!currentBox->game->tab_damier[(row-1)*taille+col+1]->getHasPion()))
-            { 
+            {
                 //droite
                 location.append(currentBox->game->tab_damier[(row-1)*taille+col+1]);
                 boitePionEnnemi.append(nullptr);
@@ -128,14 +151,13 @@ void Pion::moves()
 
             }
             if(col > 0 && row > 0 && (!currentBox->game->tab_damier[(row-1)*taille+col-1]->getHasPion()))
-            { 
+            {
                 //gauche
                 location.append(currentBox->game->tab_damier[(row-1)*taille+col-1]);
                 boitePionEnnemi.append(nullptr);
                 //boxSetting(location.last());
             }
         }
-
     }
     else //pour les pions noirs
     {
@@ -143,7 +165,7 @@ void Pion::moves()
         {
             //qqch a manger a gauche ?
             if(!currentBox->game->tab_damier[(row+2)*taille+col-2]->getHasPion())
-            { 
+            {
                 //on verifie ya pas de pion sur point de chute
                 location.append(currentBox->game->tab_damier[(row+2)*taille+col-2]);
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row+1)*taille+col-1]);
@@ -154,7 +176,7 @@ void Pion::moves()
         {
             //qqch a manger a droite ?
             if(!currentBox->game->tab_damier[(row+2)*taille+col+2]->getHasPion())
-            { 
+            {
                 //on verifie ya pas de pion sur point de chute
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row+1)*taille+col+1]);
                 location.append(currentBox->game->tab_damier[(row+2)*taille+col+2]);
@@ -165,7 +187,7 @@ void Pion::moves()
         {
             //qqch a manger a gauche arriere ?
             if(!currentBox->game->tab_damier[(row-2)*taille+col-2]->getHasPion())
-            { 
+            {
                 //on verifie ya pas de pion sur point de chute
                 location.append(currentBox->game->tab_damier[(row-2)*taille+col-2]);
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row-1)*taille+col-1]);
@@ -176,7 +198,7 @@ void Pion::moves()
         {
             //qqch a manger a droite arriere ?
             if(!currentBox->game->tab_damier[(row-2)*taille+col+2]->getHasPion())
-            { 
+            {
                 //on verifie ya pas de pion sur point de chute
                 boitePionEnnemi.append(currentBox->game->tab_damier[(row-1)*taille+col+1]);
                 location.append(currentBox->game->tab_damier[(row-2)*taille+col+2]);
@@ -188,7 +210,7 @@ void Pion::moves()
         if(location.size()==0 && boitePionEnnemi.size()==0)
         {
             if(row< taille-1 && col>0 && (!currentBox->game->tab_damier[(row+1)*taille+col-1]->getHasPion()))
-            { 
+            {
                 //gauche
                 qDebug()<<"fonction moves() possibilite d'avancer a gch qd3";
                 location.append(currentBox->game->tab_damier[(row+1)*taille+col-1]);
@@ -196,7 +218,7 @@ void Pion::moves()
                 //boxSetting(location.last());
             }
             if(row< taille-1 && col<taille-1 && (!currentBox->game->tab_damier[(row+1)*taille+col+1]->getHasPion()))
-            { 
+            {
                 //droite
                 qDebug()<<" fonction moves() possibilite d'avancer a dte qd4";
                 location.append(currentBox->game->tab_damier[(row+1)*taille+col+1]);
@@ -208,6 +230,7 @@ void Pion::moves()
     }
 
 }
+
 void Pion::setCurrentBoite(Boite *box)
 {
 
@@ -224,7 +247,7 @@ Couleur Pion::getSide()
     return side;
 }
 
-void Pion::setSide( Couleur value)
+void Pion::setSide(Couleur value)
 {
     side = value;
 }
