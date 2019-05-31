0f25e29adfd8a5f94578c2cd4e882c8904991f79
karolinasea <46023630+karolinasea@users.noreply.github.com>
2019-05-31 03:25:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,17 @@ Game *game;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    game = new Game();
-    game->showMaximized(); //showMaximized Shows the widget maximized.
-    game->displayMainMenu();
-    return a.exec();
+    int result;
+    do {
+
+        game = new Game();
+        game->showMaximized(); //showMaximized Shows the widget maximized.
+        game->displayMainMenu();
+        result= a.exec();
+        game->hide();
+    } while(result==2000);
+    return result;
+
 }
 
 
